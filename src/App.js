80e07b04d30d87a584d44d882cedbe2aa8390aef
Max <maxjoyce99@gmail.com>
2024-05-19 17:47:34 -0400

    Added budget tracking functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 //Code to import Budget.js
 import Budget from './components/Budget';
+import Remaining from './components/Remaining';
 
 // Add code to import the other components here under
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
+import AllocationForm from './components/AllocationForm';
+import RemainingBudget from './components/Remaining';
 
 
 import { AppProvider } from './context/AppContext';
@@ -17,29 +21,43 @@ const App = () => {
                     <div className='row mt-3'>
                         {
                             /* Add Budget component here */
+                         // Budget component
+                        <div className='col-sm'>
+                        <Budget />
+                        </div>
                         }        
 
                         {
                             /* Add Remaining component here*/
+                                             //Remaining component
+                            <div className='col-sm'>
+                            <Remaining />
+                        </div>
                         }        
 
                         {
+                            
                             /* Add ExpenseTotal component here */
-                        }        
-                       
-                        {
-                            /* Add ExpenseList component here */
-                        }         
-
-                        {
-                            /* Add ExpenseItem component here */
-                        }        
-
-                        {
-                            /* Add AllocationForm component here under */
-                        }        
-
+                                             //ExpenseTotal component
+                            <div className='col-sm'>
+                            <ExpenseTotal />
+                        </div>
+                        }             
                 </div>
+                <h3 className='mt-3'>Allocation</h3>
+                <div className='row '>
+                    <div className='col-sm'>
+                        <ExpenseList />
+                    </div>
+                </div>
+                <h3 className='mt-3'>Change allocation</h3>
+                <div className='row mt-3'>
+                    <div className='col-sm'>
+                        <AllocationForm/>
+                </div>
+                </div>
+
+
             </div>
         </AppProvider>
     );
